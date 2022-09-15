998770ff6b1205175204d818449b9ef71c95205c
iRealWorlds <marincalin2551@hotmail.com>
2022-09-15 17:39:24 +0300

    Added a service provider.

---
+++ src/Providers/AuthorizationServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Codestage\Authorization\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AuthorizationServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+    }
+}
