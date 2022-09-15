e9758170305485df973c4c4a6e091ce8bcb7265c
iRealWorlds <marincalin2551@hotmail.com>
2022-09-16 01:44:59 +0300

    The install command should not update the Kernel class as well.

--- src/Console/Commands/InstallCommand.php
+++ src/Console/Commands/InstallCommand.php
@@ -2,14 +2,19 @@
 
 namespace Codestage\Authorization\Console\Commands;
 
+use Codestage\Authorization\Middleware\CheckAuthorizationMiddleware;
 use Codestage\Authorization\Providers\AuthorizationServiceProvider;
+use Error;
 use Illuminate\Console\Command;
+use ReflectionClass;
 use Symfony\Component\Console\Command\Command as BaseCommand;
 
 class InstallCommand extends Command
 {
     const PermissionEnumPath = 'app/Enums/Permission.php';
     const PermissionEnumNamespace = 'App\Enums';
+    const KernelPath = 'app/Http/Kernel.php';
+    const KernelClass = '\App\Http\Kernel::class';
 
     /**
      * The name and signature of the console command.
@@ -61,6 +66,18 @@ class InstallCommand extends Command
             $this->call('migrate');
         }
 
+        // Add the middleware to the Kernel
+        if ($this->addMiddlewareToKernel()) {
+            $this->output->success('Middleware added to the HTTP Kernel file.');
+        } else {
+            $this->output->warning([
+                'Middleware could not be added to the Kernel file!',
+                'You will have to do this yourself manually.',
+                'We suggest you place it in your middleware groups, before SubstituteBindings is run',
+                'Note that placing it in the global middleware will not work.'
+            ]);
+        }
+
         return BaseCommand::SUCCESS;
     }
 
@@ -84,13 +101,91 @@ class InstallCommand extends Command
             }
         }
 
-        // Get the stub contents
-        $stub = file_get_contents($this->laravel->basePath('vendor/codestage/laravel-authorization/stubs/permission_enum.stub'));
-
-        // Replace the namespace
-        $stub = str_ireplace('{{ namespace }}', self::PermissionEnumNamespace, $stub);
+        // Generate the permission enum from the stub
+        $enumContents = $this->completeStub('permission_enum.stub', [
+            'namespace' => self::PermissionEnumNamespace
+        ]);
 
         // Publish the stub
-        return file_put_contents($path, $stub) !== false;
+        return file_put_contents($path, $enumContents) !== false;
+    }
+
+    /**
+     * Generate a hydrated string value from the given stub.
+     *
+     * @param string $stubName
+     * @param array<string, string> $replacements
+     * @return string
+     */
+    private function completeStub(string $stubName, array $replacements = []): string
+    {
+        // Try to get the stub file contents
+        $stub = file_get_contents($this->laravel->basePath('vendor/codestage/laravel-authorization/stubs/' . $stubName));
+
+        // Make sure the stub actually exists
+        if ($stub === false) {
+            throw new Error('Stub not found: ' . $stubName);
+        }
+
+        // Replace interpolations from the replacements array
+        foreach ($replacements as $key => $value) {
+            $stub = (string)str_ireplace('{{ ' . $key . ' }}', $value, $stub);
+            $stub = (string)str_ireplace('{{' . $key . '}}', $value, $stub);
+        }
+
+        // Return the resulting string
+        return $stub;
+    }
+
+    /**
+     * Add the required middleware to the Kernel.
+     *
+     * @return bool
+     */
+    public function addMiddlewareToKernel(): bool
+    {
+        // If the kernel does not exist, there is nothing to be done
+        if (!file_exists(self::KernelPath)) {
+            return false;
+        }
+
+        // If the kernel does not exist, there is nothing to be done
+        if (!class_exists(self::KernelClass)) {
+            return false;
+        }
+
+        // Try to get the initial file contents
+        $newFileContents = file_get_contents(self::KernelPath);
+
+        if ($newFileContents === false) {
+            return false;
+        }
+
+        // Try to reflect on the middlewareGroups property of the Kernel class
+        $reflection = new ReflectionClass(self::KernelClass);
+
+        if (!$reflection->hasProperty('middlewareGroups')) {
+            return false;
+        }
+
+        $groups = $reflection->getProperty('middlewareGroups')->getValue();
+
+        if (!is_array($groups)) {
+            return false;
+        }
+
+        // Add the middleware to each group
+        foreach ($groups as $group => $groupMiddleware) {
+            if (!in_array(CheckAuthorizationMiddleware::class, $groupMiddleware)) {
+                $newFileContents = preg_replace(
+                    '([\'"]' . $group . '[\'"] => \[[a-zA-Z0-9\s:,/\'"]+?)((?:\w|\)+)?SubstituteBindings::class)',
+                    '$1' . CheckAuthorizationMiddleware::class . ',\n\t$2',
+                    $newFileContents
+                );
+            }
+        }
+
+        // Update the kernel file
+        return file_put_contents(self::KernelPath, $newFileContents) !== false;
     }
 }
