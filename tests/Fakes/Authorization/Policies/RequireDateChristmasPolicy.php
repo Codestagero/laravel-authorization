166b334e894ff4caf48c94369cbc9671b3e334f4
iRealWorlds <marincalin2551@hotmail.com>
2022-09-16 14:56:43 +0300

    Added tests for Policy based authorization.

---
+++ tests/Fakes/Authorization/Policies/RequireDateChristmasPolicy.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Codestage\Authorization\Tests\Fakes\Authorization\Policies;
+
+use Carbon\Carbon;
+use Codestage\Authorization\Contracts\IPolicy;
+use Codestage\Authorization\Tests\Fakes\Authorization\Requirement\DateRequirement;
+
+class RequireDateChristmasPolicy implements IPolicy
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function requirements(): array
+    {
+        $christmas = Carbon::today()->setDay(25)->setMonth(12);
+
+        return [
+            new DateRequirement($christmas)
+        ];
+    }
+}
